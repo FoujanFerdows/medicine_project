5e8094bc989b03ab55563c0d6e7b5ff851e81667
Foujan Ferdows <foujanferdows@Foujans-Air.home>
2025-02-09 13:30:30 +0100

    Initial commit

---
+++ catalog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+# Medicine model
+class Medicine(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    active_ingredients = models.TextField()
+    usage_instructions = models.TextField()
+    side_effects = models.TextField()
+    availability = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+  
