105348273cb4e78f3f78ba95be9063ee78766bb5
Foujan Ferdows <foujanferdows@Foujans-Air.home>
2025-02-12 19:06:04 +0100

    git push origin main Update remote repository URL and other necessary changes
    wq

--- medicine_catalog/views.py
+++ medicine_catalog/views.py
@@ -1,5 +1,15 @@
 
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Medicine
 
+# Home view
 def home(request):
-    return HttpResponse("Welcome to the Medicine Web Application!")
+    return HttpResponse("<h1>Welcome to the Medicine App!</h1>")
+
+# Medicine detail view
+def medicine_detail(request, pk):
+    # Get the Medicine object or return 404 if not found
+    medicine = get_object_or_404(Medicine, pk=pk)
+    
+    # Render the medicine detail template with the medicine object
+    return render(request, 'medicine_detail.html', {'medicine': medicine})
