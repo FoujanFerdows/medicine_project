1262cdd422768029078f30ef00a21d7ce133b740
FoujanFerdows <foujanferdows@gmail.com>
2025-02-12 09:36:11 +0100

    Update urls.py

--- medicine_app/urls.py
+++ medicine_app/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from.import views  
+from . import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),  
     path('medicine/<int:pk>/', views.medicine_detail, name='medicine_detail'), 
 ]
-
