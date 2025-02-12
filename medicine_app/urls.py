45af185b0a40b193fd40e2ef55c312ab03d49344
FoujanFerdows <foujanferdows@gmail.com>
2025-02-12 09:50:22 +0100

    Update urls.py

--- medicine_app/urls.py
+++ medicine_app/urls.py
@@ -1,9 +1,11 @@
+# urls.py
+
 from django.contrib import admin
 from django.urls import path
-from . import views  
+from . import views  # Import views from the current directory
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  
-    path('medicine/<int:pk>/', views.medicine_detail, name='medicine_detail'), 
+    path('', views.home, name='home'),  # Map the home view to the root URL
+    path('medicine/<int:pk>/', views.medicine_detail, name='medicine_detail'),  # Map medicine detail view
 ]
