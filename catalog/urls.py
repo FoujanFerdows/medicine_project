4b99afb68a0bb67fc7706fdca7db60af28b09d26
FoujanFerdows <foujanferdows@gmail.com>
2025-02-16 11:17:21 +0100

    Create urls.py

---
+++ catalog/urls.py
@@ -0,0 +1,9 @@
+# catalog/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('medicine/<int:pk>/', views.medicine_detail, name='medicine_detail'),
+]
