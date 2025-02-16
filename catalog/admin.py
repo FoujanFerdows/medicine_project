92ad8f9a9b3abb35102fec748804e16c73bc76ae
FoujanFerdows <foujanferdows@gmail.com>
2025-02-16 11:03:19 +0100

    Update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Medicine, Category
+from catalog.models import Medicine, Category
 
 admin.site.register(Medicine)
 admin.site.register(Category)
