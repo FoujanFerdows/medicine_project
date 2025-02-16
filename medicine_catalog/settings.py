6fde6fa2a4002af4e7f0818924e48f68d46fa85c
FoujanFerdows <foujanferdows@gmail.com>
2025-02-16 20:02:36 +0100

    Update settings.py

--- medicine_catalog/settings.py
+++ medicine_catalog/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
-'a-very-secret-key-that-you-can-use-for-now'
+SECRET_KEY = 'a-very-secret-key-that-you-can-use-for-now'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
