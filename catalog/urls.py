8e65df846b059abf06ed515af6850ab3f91aeb50
FoujanFerdows <foujanferdows@gmail.com>
2025-02-17 14:06:10 +0100

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # This maps to your home view
-    path('medicine/<int:pk>/', views.medicine_detail, name='medicine_detail'),  # This maps to the medicine detail view
+    path('', views.home, name='home'),  
+    path('medicine/<int:pk>/', views.medicine_detail, name='medicine_detail'),  
 ]
