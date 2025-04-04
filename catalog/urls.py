6e96aa3c68d93886022e1e880c5014c21cbefc49
Foujan Ferdows <foujanferdows@Foujans-Air.home>
2025-04-04 18:21:33 +0200

    Your commit message here

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  
     path('medicine/<int:pk>/', views.medicine_detail, name='medicine_detail'),  
+    path('about/', views.about, name='about'),  # Add About Us page route
 ]
