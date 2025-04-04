fb439a35cce9ab36d65d7d8a09e3fe6c97d02824
FoujanFerdows <foujanferdows@gmail.com>
2025-04-04 18:06:54 +0200

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  
     path('medicine/<int:pk>/', views.medicine_detail, name='medicine_detail'),  
-]
-urlpatterns = [
     path('about/', views.about, name='about'),  # Add About Us page route
 ]
