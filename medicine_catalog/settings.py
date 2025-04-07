9215dea73b580b380cec8c262727f02046dd3972
FoujanFerdows <foujanferdows@gmail.com>
2025-04-07 12:13:08 +0200

    Update settings.py

--- medicine_catalog/settings.py
+++ medicine_catalog/settings.py
@@ -102,6 +102,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Define the path where your static files are located
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
