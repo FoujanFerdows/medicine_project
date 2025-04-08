08d95b3d8a81764a75dd5802957f7dc9bf071f24
FoujanFerdows <foujanferdows@gmail.com>
2025-04-08 22:35:00 +0200

    Update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from catalog.models import Medicine, Category
+from catalog.models import Medicine, Category, Symptom  # Import Symptom model
 
+# Register your models
 admin.site.register(Medicine)
 admin.site.register(Category)
-
+admin.site.register(Symptom)  # Register the Symptom model
