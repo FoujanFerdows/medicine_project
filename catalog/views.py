0a0206d634467d66a3b710f11a231bdc519f752a
TK523-prog <khat31204@gmail.com>
2025-04-23 01:13:04 +0200

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Medicine, Symptom, ShoppingList  # Import Symptom model for symptom search
+from django.core.mail import send_mail
+from django.conf import settings
+
+from .forms import ContactForm
+from .models import ContactSubmission
 
 # Home view to display all medicines or search results
 def home(request):
