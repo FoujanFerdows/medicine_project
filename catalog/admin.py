f9ff2963e3e82fbea19c7cd0a76c275777d8886c
TK523-prog <khat31204@gmail.com>
2025-04-23 01:01:25 +0200

    Update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from catalog.models import Medicine, Category, Symptom  
-
+from .models import ContactSubmission
 
 admin.site.register(Medicine)
 admin.site.register(Category)
 admin.site.register(Symptom) 
+@admin.register(ContactSubmission)
+class ContactSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'email', 'submitted_at')
+    list_filter  = ('submitted_at',)
+    search_fields = ('title', 'email', 'description') 
