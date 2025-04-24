685f5839c09d58200617237a8be0b79635363157
TK523-prog <khat31204@gmail.com>
2025-04-24 02:16:32 +0200

    Update models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 AVAILABILITY_CHOICES = [
     ('in_stock', 'In Stock'),
@@ -60,4 +62,18 @@ class ContactSubmission(models.Model):
 
     def __str__(self):
         return f"{self.title} from {self.email}"
+
+class Profile(models.Model):
+    user           = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    health_status  = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
+
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
 
