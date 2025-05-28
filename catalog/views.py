a18191eefdeaff0e6f8d9725a380c92a3a9f127d
TK523-prog <khat31204@gmail.com>
2025-05-28 02:04:43 +0200

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,8 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Medicine, Symptom, ShoppingList  # Import Symptom model for symptom search
+from django.core.mail import send_mail
+from django.conf import settings
+
 from .forms import ContactForm
 from .models import ContactSubmission
 
