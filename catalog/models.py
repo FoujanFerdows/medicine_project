b00237dbc31264b722f642c83571138dfb6c2ff1
TK523-prog <khat31204@gmail.com>
2025-05-31 02:18:35 +0200

    kha changes

--- catalog/models.py
+++ catalog/models.py
@@ -12,6 +12,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    class Meta:
+        verbose_name_plural = "Categories"
+    
     def __str__(self):
         return self.name
 
